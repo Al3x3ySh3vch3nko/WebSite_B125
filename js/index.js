5ff1d23754a41ca34bdd424638068f1fc2a70c4d
Al3x3ySh3vch3nko <a.r.shevchenko@gmail.com>
2021-11-07 22:55:50 +0300

    +

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(function()
 {
-    $('.slider__inner').slick
+    $('.slider__inner, .news__slider-inner').slick
     ({
         nextArrow: '<button type="button" class="slider-arrow slick-next icon-angle-right"></button>',
         prevArrow: '<button type="button" class="slider-arrow slick-prev icon-angle-left"></button>'
